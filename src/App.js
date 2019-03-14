4ed559ae122e355714322d0615b8b15cbfc853ea
smitchell-gt <steven.mitchell.tkd@gmail.com>
2019-03-13 21:45:22 -0700

    Adds error message with empty search

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-import SearchField from 'react-search-field';
+import SearchComponent from './SearchComponent';
 import './App.css';
 
 class App extends Component {
-  handleSearch = searchText => {
-    console.log('making a search:', searchText);
-  };
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <SearchField
-            placeholder="Search here..."
-            onEnter={this.handleSearch}
-            onSearchClick={this.handleSearch}
-          />
+          <SearchComponent />
         </header>
       </div>
     );
---
+++ src/SearchComponent.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import SearchField from 'react-search-field';
+
+class SearchComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEmptySearch: false,
+      searchData: ''
+    }
+  }
+
+  handleSearch = searchText => {
+    console.log('making a search:', searchText);
+    if (!searchText) {
+      this.setState({isEmptySearch: true});
+    } else {
+      this.setState({
+        isEmptySearch: false,
+        searchData: `Tweet Data for search: ${searchText}`
+      });
+    }
+  };
+
+  render() {
+    const renderContents =
+      this.state.isEmptySearch
+        ? <div style={{'color': 'red'}}>Please make a non-empty search.</div>
+        : this.state.searchData;
+    return(
+      <div>
+        <SearchField
+          placeholder="Search here..."
+          onEnter={this.handleSearch}
+          onSearchClick={this.handleSearch}
+        />
+        <br /><br />
+        { renderContents }
+      </div>
+    );
+  }
+}
+
+export default SearchComponent;
